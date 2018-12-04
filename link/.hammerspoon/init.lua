eef7354b9d14f8d6a1d441dc1a4e08086b275cdb
Akeda Bagus <akeda.bagus@automattic.com>
2018-12-04 16:07:24 +0700

    Activate miro windows manager.

--- link/.hammerspoon/init.lua
+++ link/.hammerspoon/init.lua
@@ -1,3 +1,13 @@
 -- Miro's windows manager.
+local hyper = {"ctrl", "alt", "cmd"}
 
 hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0.3
+spoon.MiroWindowsManager:bindHotkeys({
+	up = {hyper, "k"},
+	down = {hyper, "j"},
+	right = {hyper, "l"},
+	left = {hyper, "h"},
+	fullscreen = {hyper, "i"}
+})
