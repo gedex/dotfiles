ed814d401b6f945a2007351081677fcd0655953d
Akeda Bagus <akeda.bagus@automattic.com>
2020-10-19 15:10:39 +0700

    Always install and/or use latest nodejs

--- source/50_node.sh
+++ source/50_node.sh
@@ -7,8 +7,8 @@ PATH=~/.nave/installed/default/bin:"$(path_remove ~/.nave/installed/*/bin)"
 function nave_default() {
   local version
   local default=${NAVE_DIR:-$HOME/.nave}/installed/default
-  [[ ! "$1" ]] && echo "Specify a node version or \"stable\"" && return 1
-  [[ "$1" == "stable" ]] && version=$(nave stable) || version=${1#v}
+  [[ ! "$1" ]] && echo "Specify a node version or \"latest\"" && return 1
+  [[ "$1" == "latest" ]] && version=$(nave latest) || version=${1#v}
   rm "$default" 2>/dev/null
   ln -s $version "$default"
   echo "Nave default set to $version"
@@ -17,13 +17,13 @@ function nave_default() {
 # Install a version of node, set as default, install npm modules, etc.
 function nave_install() {
   local version
-  [[ ! "$1" ]] && echo "Specify a node version or \"stable\"" && return 1
-  [[ "$1" == "stable" ]] && version=$(nave stable) || version=${1#v}
+  [[ ! "$1" ]] && echo "Specify a node version or \"latest\"" && return 1
+  [[ "$1" == "latest" ]] && version=$(nave latest) || version=${1#v}
   if [[ ! -d "${NAVE_DIR:-$HOME/.nave}/installed/$version" ]]; then
     e_header "Installing Node.js $version"
     nave install $version
   fi
-  [[ "$1" == "stable" ]] && nave_default stable && npm_install
+  [[ "$1" == "latest" ]] && nave_default latest && npm_install
 }
 
 # Use the version of node in the local .nvmrc file
